c6b00fd6a16490e938e0bed4f13241a7296fa017
Mohammad Suhail Shaik <suhailshaikmohammad129@gmail.com>
2025-12-30 22:59:58 +0530

    u8

--- server/Models/Employees.js
+++ server/Models/Employees.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+  const mongoose = require("mongoose");
+  const bcrypt=require("bcrypt")
 
-const employeeSchema = mongoose.Schema(
-  {
+
+  const employeeSchema = mongoose.Schema({
     firstName: {
       type: String,
       required: true,
@@ -21,103 +21,67 @@ const employeeSchema = mongoose.Schema(
     location: {
       type: String,
       required: true,
-      set: (v) => v ? v.toLowerCase().trim() : v, 
     },
     language: {
       type: String,
       required: true,
-      set: (v) => v ? v.toLowerCase().trim() : v, 
     },
-    assignedLeads: [
+    assignedLeads:[
       {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Lead",
-      },
-    ],
-    closedLeads: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Lead",
-      },
-    ],
+      type: mongoose.Schema.Types.ObjectId,
+      ref:"Lead",
+    }],
+    closedLeads: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref:"Lead",
+    }],
     status: {
       type: String,
       default: "inactive",
     },
     activityLog: [
-      {
-        type: {
-          type: String,
-          enum: ["LEAD_ASSIGNED", "LEAD_CLOSED", "CHECK_IN", "CHECK_OUT"],
-          required: true,
-        },
-        message: { type: String, required: true },
-        relatedId: mongoose.Schema.Types.ObjectId,
-        createdAt: { type: Date, default: Date.now },
-      },
-    ],
+  {
+    type: {
+      type: String,
+      enum: ["LEAD_ASSIGNED", "LEAD_CLOSED", "CHECK_IN", "CHECK_OUT"],
+      required: true,
+    },
+    message: { type: String, required: true },
+    relatedId: mongoose.Schema.Types.ObjectId,
+    createdAt: { type: Date, default: Date.now },
   },
-  { timestamps: true }
-);
+],
 
-employeeSchema.pre("save", function(next) {
-  if (this.language) {
-    this.language = this.language.toLowerCase().trim();
-  }
-  if (this.location) {
-    this.location = this.location.toLowerCase().trim();
-  }
-  next();
-});
+  },
+    { timestamps: true }
+  );
 
-employeeSchema.pre("findOneAndUpdate", function(next) {
-  const update = this.getUpdate();
-  
-  if (update.$set) {
-    if (update.$set.language) {
-      update.$set.language = update.$set.language.toLowerCase().trim();
+  employeeSchema.pre("validate", function () {
+    if (!this.password) {
+      this.password = this.email;
     }
-    if (update.$set.location) {
-      update.$set.location = update.$set.location.toLowerCase().trim();
-    }
-  }
-  
-  if (update.language) {
-    update.language = update.language.toLowerCase().trim();
-  }
-  if (update.location) {
-    update.location = update.location.toLowerCase().trim();
-  }
-  
-  this.setUpdate(update);
-  next();
-});
+  });
 
-employeeSchema.pre("validate", function () {
-  if (!this.password) {
-    this.password = this.email;
-  }
-});
-
-employeeSchema.pre("save", async function () {
+  employeeSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.email, salt);
-});
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.email, salt);
+  })
 
-employeeSchema.statics.login = async function (email, password) {
-  const emp = await this.findOne({ email });
 
-  if (!emp) {
-    throw new Error("Email Id Doesnot exist. Try with valid one");
+  employeeSchema.statics.login=async function(email, password){
+    const emp=await this.findOne({email})
+
+    if(!emp){
+      throw new Error ("Email Id Doesnot exist. Try with valid one")
+    }
+    const match=await bcrypt.compare(password, emp.password)
+    if(!match){
+      throw new Error ("Invalid Password, Try Again!!")
+    }
+    return emp
   }
-  const match = await bcrypt.compare(password, emp.password);
-  if (!match) {
-    throw new Error("Invalid Password, Try Again!!");
-  }
-  return emp;
-};
 
-const Employee = new mongoose.model("Employee", employeeSchema);
+  const Employee = new mongoose.model("Employee", employeeSchema);
 
-module.exports = Employee;
+  module.exports = Employee;
