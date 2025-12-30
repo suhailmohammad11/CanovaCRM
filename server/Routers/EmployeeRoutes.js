45987e6b3a29260e125e4df1bc4e04f1e9213a32
Mohammad Suhail Shaik <suhailshaikmohammad129@gmail.com>
2025-12-30 22:35:34 +0530

    u7

--- server/Routers/EmployeeRoutes.js
+++ server/Routers/EmployeeRoutes.js
@@ -15,9 +15,9 @@ const router = express.Router();
 
 router.post("/login", employeeLogin);
 router.get("/Home/Leads", AuthMiddleware, getEmployeeLeads);
-router.patch("/Home/Leads/closeLead/:id", AuthMiddleware, closeLead);
-router.patch("/Home/Lead/changeType/:id", AuthMiddleware, changeType);
 router.patch("/Home/Lead/scheduleDate/:id", AuthMiddleware, scheduleLead);
+router.patch("/Home/Lead/changeType/:id", AuthMiddleware, changeType);
+router.patch("/Home/Leads/closeLead/:id", AuthMiddleware, closeLead);
 
 //profile routes
 router.put("/Home/Profile/:id", AuthMiddleware, editProfile);
